6cc33d685b6ce413ef0a3c2872631dce0bfa0a74
Anish <59597772+GA352491@users.noreply.github.com>
2021-03-01 16:28:18 +0530

    Add files via upload

--- elearning/static/blogs/blog.js
+++ elearning/static/blogs/blog.js
@@ -1,23 +1,23 @@
 
-tinymce.init({
-  selector: 'textarea#local-upload',
-  plugins: 'image code',
-  toolbar: 'undo redo | image code',
+// tinymce.init({
+//   selector: 'textarea#local-upload',
+//   plugins: 'image code',
+//   toolbar: 'undo redo | image code',
+//
+//   /* without images_upload_url set, Upload tab won't show up*/
+//   images_upload_url: 'postAcceptor.php',
+//
+//   /* we override default upload handler to simulate successful upload*/
+//   images_upload_handler: function (blobInfo, success, failure) {
+//     setTimeout(function () {
+//       /* no matter what you upload, we will turn it into TinyMCE logo :)*/
+//       success('http://moxiecode.cachefly.net/tinymce/v9/images/logo.png');
+//     }, 2000);
+//   },
+//   content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+// });
 
-  /* without images_upload_url set, Upload tab won't show up*/
-  images_upload_url: 'postAcceptor.php',
-
-  /* we override default upload handler to simulate successful upload*/
-  images_upload_handler: function (blobInfo, success, failure) {
-    setTimeout(function () {
-      /* no matter what you upload, we will turn it into TinyMCE logo :)*/
-      success('http://moxiecode.cachefly.net/tinymce/v9/images/logo.png');
-    }, 2000);
-  },
-  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-});
-
-
+  <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 
 
 
