46c99fe6f4be06ecdc9006067378d1e3100481f8
Anish <59597772+GA352491@users.noreply.github.com>
2021-03-15 13:57:42 +0530

    Add files via upload

--- elearning/models.py
+++ elearning/models.py
@@ -3,27 +3,26 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.conf import settings
 
-
-class Payment(models.Model):
-    user_name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100, null=True, default='')
-    course = models.CharField(max_length=100, null=True)
-    secondary_course = models.CharField(max_length=100, null=True)
-    amount = models.IntegerField(null=True)
-    payment_id = models.CharField(max_length=100)
-    paid = models.BooleanField(default=False)
-    date_of_payment = models.DateField(null=True)
-
-    def __str__(self):
-        return self.user_name
-
-
-class Courses(models.Model):
-    course_name = models.CharField(max_length=100)
-    price = models.IntegerField(null=True)
-
-    def __str__(self):
-        return self.course_name
+# class Payment(models.Model):
+#     user_name = models.CharField(max_length=100)
+#     email = models.CharField(max_length=100, null=True, default='')
+#     course = models.CharField(max_length=100, null=True)
+#     secondary_course = models.CharField(max_length=100, null=True)
+#     amount = models.IntegerField(null=True)
+#     payment_id = models.CharField(max_length=100)
+#     paid = models.BooleanField(default=False)
+#     date_of_payment = models.DateField(null=True)
+#
+#     def __str__(self):
+#         return self.user_name
+#
+#
+# class Courses(models.Model):
+#     course_name = models.CharField(max_length=100)
+#     price = models.IntegerField(null=True)
+#
+#     def __str__(self):
+#         return self.course_name
 
 
 from tinymce import HTMLField
@@ -38,3 +37,19 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class FaqCategory(models.Model):
+    category = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.category
+
+
+class Faq(models.Model):
+    category = models.ForeignKey(FaqCategory, on_delete=models.CASCADE, null=True)
+    question = models.CharField(max_length=400, null=True, blank=True)
+    solution = models.TextField(max_length=3000, null=True, blank=True)
+
+    def __str__(self):
+        return self.question
